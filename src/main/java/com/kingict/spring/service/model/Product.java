78c71c06d822e6dcc5c0d46e3f2e166ed093e791
Petar Štika <stika.petar@gmail.com>
2024-06-22 20:14:46 +0200

    Add Product DTO

--- src/main/java/com/kingict/spring/service/model/Product.java
+++ src/main/java/com/kingict/spring/service/model/Product.java
@@ -1,4 +1,69 @@
 package com.kingict.spring.service.model;
 
 public class Product {
+    private Long id;
+    private String name;
+    private String category;
+    private String description;
+    private Double price;
+    private byte[] imageData;
+
+    // empty constructor
+    public Product() { }
+
+    public Product(Long id, String name, String category, String description, Double price) {
+        this.id = id;
+        this.name = name;
+        this.category = category;
+        this.description = description;
+        this.price = price;
+    }
+
+    public byte[] getImageData() {
+        return imageData;
+    }
+
+    public void setImageData(byte[] imageData) {
+        this.imageData = imageData;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
